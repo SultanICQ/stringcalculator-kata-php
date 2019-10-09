8406841a51dfb3df8fbe93aacec8c67b3d25e3ee
David Garcia Latorre <dgarcia@seocom.agency>
2019-10-09 23:32:13 +0200

    Upgrade to PHPUnit 8 and Project adaptation to the Kata name

---
+++ src/StringCalculator/StringCalculatorClass.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace StringCalculator;
+
+class StringCalculatorClass
+{
+}
--- src/StringCalculator/Tests/StringCalculatorTest.php
+++ src/StringCalculator/Tests/StringCalculatorTest.php
@@ -1,13 +1,16 @@
 <?php
+declare(strict_types=1);
 
-class DummyClassTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
 {
     /**
      * @test
      */
     public function the_tests_can_be_run_and_classes_are_available_via_the_composer_autoloader()
     {
-        if (!class_exists('DummyProject\DummyClass')) {
+        if (!class_exists('StringCalculator\StringCalculator    Class')) {
             $this->fail('Make sure you use the "phpunit.xml.dist" from this project as the configuration file for your PHPUnit test runner');
         }
     }
