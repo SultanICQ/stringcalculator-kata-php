79038d96e9435c2886fd40503d34c0ffa84d9604
David Garcia Latorre <dgarcia@seocom.agency>
2019-10-10 19:47:48 +0200

    Refactor: Removed old interfaces of parallel change refactor

--- src/StringCalculator/Tests/StringCalculatorTest.php
+++ src/StringCalculator/Tests/StringCalculatorTest.php
@@ -19,29 +19,6 @@ class StringCalculatorTest extends TestCase
         $this->assertIsObject($this->calc);
     }
 
-    public function test_when_i_send_empty_arguments_calc_returns_zero_int() {
-
-        $sum = $this->calc->Add("");
-
-        $this->assertIsInt($sum);
-        $this->assertSame(0, $sum);
-    }
-    public function test_when_i_send_a_string_with_the_number_one_calc_returns_the_same_number_as_int() {
-
-        $sum = $this->calc->Add("1");
-
-        $this->assertIsInt( $sum );
-        $this->assertSame( 1, $sum );
-    }
-
-    public function test_when_i_send_a_string_with_the_number_two_calc_returns_the_same_number_as_int() {
-
-        $sum = $this->calc->Add("2");
-
-        $this->assertIsInt( $sum );
-        $this->assertSame( 2, $sum );
-    }
-
     /**
      * @dataProvider single_number_provider
      * @param String $argument
