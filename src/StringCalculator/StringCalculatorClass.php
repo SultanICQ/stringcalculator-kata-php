6b5a3f6c8dad6523721f05a9592b7ce788a9b38c
David Garcia Latorre <dgarcia@seocom.agency>
2019-10-10 19:29:47 +0200

    Refactor: We generalize the conversion of input arguments to int

--- src/StringCalculator/StringCalculatorClass.php
+++ src/StringCalculator/StringCalculatorClass.php
@@ -6,8 +6,6 @@ class StringCalculatorClass
 {
     public function Add(string $string):Int
     {
-        if ( $string == "2" ) { return 2; }
-        if ( $string == "1" ) { return 1; }
-        return 0;
+        return (int)$string;
     }
 }
